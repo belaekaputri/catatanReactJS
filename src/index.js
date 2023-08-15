1d3df9d35fb2575d70b8969f2be06faeb89b0b16
belaekaputri <belaekaputri28@gmail.com>
2023-08-15 13:50:24 +0700

    class component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import App from "./components/App"
  
-//membuat komponen
+/*membuat komponen
 const App = () => {
     return(
         <div>
             <h1>Hello World</h1>
         </div>
     )
-}
+}*/
 
 ReactDOM.render(
+    <React.StrictMode>
     <App/>,
+    </React.StrictMode>,
     document.getElementById("root")
 )
+//react strictmode digunakan apabila ada error atau modul yang tidak support makan akan diberitahu
