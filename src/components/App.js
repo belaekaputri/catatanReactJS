8e5d832ebb754d95f43a45d81192e76a9f401616
belaekaputri <belaekaputri28@gmail.com>
2023-08-15 14:14:26 +0700

    JSX

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ class App extends React.Component{
     return(
         <div>
             <h1>Hello World</h1>
+            <p>Sekilas mirip dengan html tapi di dalam div ini adalah JSX yang saratnya ada satu root yaitu div</p>
         </div>
     )
   }
